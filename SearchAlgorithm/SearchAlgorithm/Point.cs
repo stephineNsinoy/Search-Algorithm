adb157dcc365c658038b036219c6ca13ee5dbd19
Stephine N. Sinoy <stephine.sinoy@cit.edu>
2022-11-29 23:21:51 +0800

    Implement Point class

--- SearchAlgorithm/SearchAlgorithm/Point.cs
+++ SearchAlgorithm/SearchAlgorithm/Point.cs
@@ -2,5 +2,16 @@
 {
     public class Point
     {
+       public int x { get; set; }
+       public int y { get; set; }
+       public int shortestPathDistance { get; set; }
+
+        public Point() { }
+       public Point(int x, int y, int distance)
+       {
+            this.x = x;
+            this.y = y;
+            this.shortestPathDistance = distance;
+       }
     }
 }
