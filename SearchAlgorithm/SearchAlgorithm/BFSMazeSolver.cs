c31fcd7e554dbea9aa37b5ad584140fdc044f963
Stephine N. Sinoy <stephine.sinoy@cit.edu>
2022-11-29 23:23:27 +0800

    Add necessary properties

--- SearchAlgorithm/SearchAlgorithm/BFSMazeSolver.cs
+++ SearchAlgorithm/SearchAlgorithm/BFSMazeSolver.cs
@@ -4,6 +4,20 @@ namespace SearchAlgorithm
 {
     public class BFSMazeSolver
     {
-        
+        /// <summary>
+        /// Stores the shortest path from the starting point to the goal
+        /// </summary>
+        public List<Point> path = new List<Point>();
+
+        /// <summary>
+        /// Stores state and origin for backtracking from the goal back to the starting point with the shortest path
+        /// </summary>
+        private Dictionary<Point, Point> previous = new Dictionary<Point, Point>();
+
+        public List<Point> Path
+        {
+            get { return path; } 
+            set { path = value; }
+        }
     }
 }
