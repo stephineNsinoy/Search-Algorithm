82e1699eae743a5444c3736f83bc13209adf7ffc
Stephine N. Sinoy <stephine.sinoy@cit.edu>
2022-11-29 23:24:07 +0800

    Create and implement SolveMaze method

--- SearchAlgorithm/SearchAlgorithm/BFSMazeSolver.cs
+++ SearchAlgorithm/SearchAlgorithm/BFSMazeSolver.cs
@@ -19,5 +19,58 @@ namespace SearchAlgorithm
             get { return path; } 
             set { path = value; }
         }
+
+        /// <summary>
+        /// Uses Breadth-First Search in finding the goal
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns>Shortest path distance from start to goal, -1 if goal can't be reached</returns>
+        public int SolveMaze(string[,] matrix)
+        {
+            Point start = new Point(0, 0, 0);
+            Queue<Point> queue = new Queue<Point>();
+
+            int numOfRows = matrix.GetLength(0);
+            int numOfColumns = matrix.GetLength(1);
+
+            bool[,] visited = new bool[numOfRows, numOfColumns];
+
+            // set visited matrices to false;
+            for (int x = 0; x < visited.GetLength(0); x += 1)
+            {
+                for (int y = 0; y < visited.GetLength(1); y += 1)
+                {
+                    visited[x, y] = false;
+                }
+            }
+
+            queue.Enqueue(start);
+
+            while (queue.Count() > 0)
+            {
+                Point current = queue.Dequeue();
+
+                int index = matrix[current.x, current.y].IndexOf("G");
+                if (index != -1)
+                {
+                    ShortestPath(start, current);
+                    DisplayMaze(matrix);
+                    return current.shortestPathDistance;
+                }
+                else
+                {
+                    visited[current.x, current.y] = true;
+
+                    List<Point> neighbors = FindNeighbors(current, numOfRows, numOfColumns, matrix, visited);
+                    foreach(var neighbor in neighbors)
+                    {
+                        queue.Enqueue(neighbor);
+                    }
+                }
+            }
+
+            DisplayMaze(matrix);
+            return -1;
+        }
     }
 }
