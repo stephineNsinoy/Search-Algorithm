27c18c30e7746a267cfffff9f30b409ccc07e593
Stephine N. Sinoy <stephine.sinoy@cit.edu>
2022-11-29 23:27:18 +0800

    Update Program.cs

--- SearchAlgorithm/SearchAlgorithm/Program.cs
+++ SearchAlgorithm/SearchAlgorithm/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BFSMazeSolver = SearchAlgorithm.BFSMazeSolver;
+
+string[,] matrix = 
+        {
+                {"1S", "2", "3", "▅", "5", "6" },
+                {"▅", "8", "▅", "10", "11", "12"},
+                {"13", "14", "▅", "16", "▅", "18"},
+                {"19", "▅", "21", "22", "▅", "24"},
+                {"25", "26", "27", "▅", "29", "30G"},
+                {"31", "▅", "33", "▅", "35", "36"},
+        };
+
+
+BFSMazeSolver solver = new BFSMazeSolver();
+
+int distance = solver.SolveMaze(matrix);
+if(distance != -1)
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine($"\nGoal reached with distance {distance} from start to goal");
+    solver.DisplayShortestPath(matrix);
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("\nGoal can't be reached!\nMaze is impossible to solve");
+    Console.ResetColor();   
+}
